dda8e51ba8299aa0df845f5c7c7bb72c39324679
Dlondonmedina <medinad@uw.edu>
2018-03-01 16:09:30 -0800

    finish player

--- src/Player.py
+++ src/Player.py
@@ -13,13 +13,26 @@ class Player(object):
         self.name = name
     
 
+    def modify_hitpoints(self, points=None):
+        self.hitpoints = self.hitpoints + points
+        if self.hitpoints > 10:
+            ut.slow_print("Hitpoints already maxed out.")
+            self.hitpoints = 10
+
+
     def add_item(self, item):
         if len(self.knapsack) < 5:
             self.knapsack.append(item)
         else:
-            ut.slow_print("I'm sorry, you must drop an item.")
-            
-    
+            ut.slow_print("I'm sorry, you must drop an item. \n Do you want to do this? (y/N)")
+            drop = input("")
+            if drop.lower() == "y":
+                self.remove_item()
+                self.knapsack.append(item)
+            else:
+                ut.slow_print("I guess you don't want this new item that much.")
+
+
     def remove_item(self, item_index=None):
         if item_index == None:
             ut.slow_print("Which item would you like to remove?")
@@ -34,6 +47,11 @@ class Player(object):
             del self.knapsack[item_index - 1]
         except IndexError:
             print("Sorry, no item at this index.")
+
+
+    
+    
+    
 
     # def add_item(self, item):
     #     if len(self.knapsack < 5):
--- src/player_test.py
+++ src/player_test.py
@@ -15,38 +15,46 @@ class PlayerTest(unittest.TestCase):
         self.player.set_name(name)
         self.assertEqual(self.player.name, name)
 
+    def test_player_can_lose_hit_points(self):
+        self.player.modify_hitpoints(-1)
+        self.assertEqual(9, self.player.hitpoints)
+
+    def test_player_can_gain_hitpoints(self):
+        self.player.hitpoints = 5
+        self.player.modify_hitpoints(2)
+        self.assertEqual(7, self.player.hitpoints)
+
+    def test_player_can_max_hitpoints(self):
+        self.player.modify_hitpoints(2)
+        self.assertEqual(10, self.player.hitpoints)
+
     def test_can_add_to_knapsack(self):
         old_k = self.player.knapsack[:]
         self.player.add_item("screwdriver")
         new_k = self.player.knapsack
         self.assertGreater(len(new_k), len(old_k))
-
-    def test_can_add_no_more_than_five_items(self):
-        for i in range(0,6):
-            self.player.add_item("blah")
-        knap = self.player.knapsack
-        self.assertLessEqual(len(knap), 5)
     
     def test_remove_item(self):
         self.player.knapsack = []
         self.player.add_item("wrench")
-        self.player.add_item("screwdriver")
-        with mock.patch('builtins.input',side_effect='1'):
+        with mock.patch('builtins.input', side_effect='1'):
             output = io.StringIO()
             sys.stdout = output
             self.player.remove_item()
             sys.stdout = sys.__stdout__
-            self.assertEqual(output.getvalue(), "Which item would you like to remove? 1: wrench 2: screwdriver")
+            self.assertEqual(output.getvalue(), "Which item would you like to remove? 1: wrench")
 
-    def test_can_add_items_after_five_if_remove_one(self):
+    def test_can_remove_item_if_extra_items(self):
         self.player.knapsack = []
         for i in range(0,5):
             self.player.add_item("blah")
-        output = io.StringIO()
-        sys.stdout = output
-        self.player.add_item("last_blah")
-        sys.stdout = sys.__stdout__
-        self.assertEqual(output.getvalue(), "I'm sorry, you must drop an item.")
+        with mock.patch('builtins.input', side_effect=['y', 5]):
+            output = io.StringIO()
+            sys.stdout = output 
+            self.player.add_item("last_blah")
+            sys.stdout = sys.__stdout__
+            self.assertEqual(self.player.knapsack, ['blah', 'blah', 'blah', 'blah', 'last_blah'])
+            
 
 if __name__ == '__main__':
     unittest.main()        
