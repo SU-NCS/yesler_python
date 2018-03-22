da6d3339768d8e73db10dcd485d366bd5dec9699
Dlondonmedina <medinad@uw.edu>
2018-03-22 14:11:50 -0700

    Working on Game and Episode classes

--- src/Episode.py
+++ src/Episode.py
@@ -1,2 +1,44 @@
+import json
+import Utils as ut
+import Creature
+
 class Episode:
-    pass
+#figure out how to pass a challenge into the creature.
+    def __init__(self, episode_data):
+        cd = episode_data["creature"]
+        self.creature = Creature(cd['name'], cd['introduction'], cd['greeting'], cd['story'])
+        self.room_description = episode_data['room_description']
+        self.treasure = episode_data['treasure']
+        self.traps = episode_data['traps']
+        self.hitpoints = episode_data['hitpoint_change']
+    
+    def describe_room(self):
+        try:
+            ut.slow_print(self.room_description)
+        except Exception as e:
+            print("Something went wrong: " + e)
+        
+    
+    def get_treasure(self):
+        if self.traps:
+            ut.slow_print(self.traps['text'])
+            self.hitpoints = self.hitpoints + self.traps['damage']
+            ut.slow_print("Your hitpoints: {}".format(self.hitpoints))
+            self.traps = None
+        else:
+            return self.treasure
+
+
+    def talk_to_creature(self):
+        self.creature.greet()
+        self.creature.describe()
+        self.creature.tell_story()
+        self.creature.offer_challenge(self.challenge)
+        
+
+
+    
+
+
+
+
---
+++ src/Game.py
@@ -0,0 +1,52 @@
+import json
+import time
+import os
+import Player
+import Episode
+
+class Game:
+
+    def __init__(self, story, challenge_path):
+        self.player = Player()
+        try:
+            with open(story) as f:
+                self.game_data = json.load(f)
+        except Exception as e:
+            print("Something went wrong with your file. I need a story: {}".format(e))
+        # Import our challenges
+        try:
+            for root, dirs, files in os.walk(challenge_path):
+                __all__ = [name.split(".")[0] for name in files if "challenge" in name]
+        except Exception as e2:
+            print("There's something wrong with your challenge path: {}".format(e))
+
+    def start_game(self):
+        counter = len(self.game_data)
+        while self.player.hitpoints > 0:
+            for episode in self.game_data:
+                counter = counter - 1
+                load_episode(episode)
+        if counter > 0:
+            outcome = "Game Over! You lose!"
+        else: 
+            outcome = "Congratulations! You win!"
+        self.end_game(outcome)    
+
+
+    def end_game(self, outcome):
+        ut.slow_print(outcome)
+        play_again = input("Would you like to play again? (y/N) ")
+        if lower(play_again) == "y":
+            start_game()
+        else:
+            print("Thanks for playing! Goodbye!")
+            time.sleep(2)
+            raise SystemExit
+    
+
+    def load_episode(self, episode):
+        episode = Episode(episode)
+    
+
+
+
