184b59ed9f57599e1c8f9b6954f58f10a961ad0c
Dlondonmedina <medinad@uw.edu>
2018-05-24 19:16:49 -0700

    modify creature and add test driver and challenges

--- src/Creature.py
+++ src/Creature.py
@@ -2,17 +2,17 @@ import Utils as ut
 
 class Creature(object):
     """Class for creatures that player will encounter."""
-    outcome = False
-    temperment = 0
-    is_hostile = False
 
     def __init__(self, name, introduction, greeting=None, story=None, 
                 pace=0.1):
+        # Initialize Creature properties.
         self.name = name
         self.introduction = introduction
         self.greeting = greeting
         self.story = story
         self.pace = pace
+        self.temperment = 0
+        self.is_hostile = False
 
     def speak(self, dialog):
         """Prints out text to the screen if creature is not hostile."""
@@ -31,36 +31,41 @@ class Creature(object):
                 ut.slow_print(dialog, self.pace)
 
     def make_hostile(self):
+        """
+        Increments hostility until creature is hostile. Episode must be
+        restarted if the creature becomes hostile.
+        """
         if self.temperment < 10:
             self.temperment += 1
         else:
             self.is_hostile = True
 
     def describe(self):
+        """Creature prints its introduction."""
         self.speak(self.introduction)
 
     def greet(self):
+        """Creature greets player"""
         fmap = {"name": self.name, "greeting": self.greeting}
         greeting = "Hello, traveller. My name is {name}! {greeting}".format_map(fmap)
         self.speak(greeting)        
         
     def tell_story(self):
+        """Creature tells a story."""
         self.speak(self.story)
     
     def offer_challenge(self, challenge):
-        while True:
-            if self.is_hostile:
-                self.speak("GRAAAAH!!!!! YOU'RE MAKING ME SO MAD!!!")
-                break
-            else:
-                self.outcome = challenge()
-                if self.outcome:
-                    self.speak("Wow! Great work! You pass!")
-                    break
-                else:
-                    self.make_hostile()
-        return self.outcome
-
+        """Creature takes challenge function and returns the outcome. Manage multiple attempts in Episode."""
+        while not self.is_hostile: # As long as the creature isn't hostile, we try the challenge.
+            self.outcome = challenge()
+            print(self.outcome)
+            print(self.temperment)
+            if self.outcome:
+                return self.outcome # if we pass the challenge we win the episode
+            else: # Otherwise increase hostility and try again.
+                self.make_hostile()
+        
+        return not self.is_hostile # Creature became too hostile and we fail. 
 
 
 
---
+++ src/test_driver.py
@@ -0,0 +1,26 @@
+import Episode
+import Creature
+import Utils as ut 
+import Player 
+import Game 
+import challenges 
+
+def challenge_1(num):
+        guess = None
+        guess = input("Give me a number between 1 and 10")
+        try:
+            guess = int(guess)
+            if guess == num:
+                return True
+            elif guess < num:
+                print("Try higher")
+            elif guess > num:
+                print("Try lower")
+        except:
+            print("That was not a number. Try again")
+        return False
+
+c = Creature.Creature("Billy", "Hi there", "No greeting", "no story")
+while True:
+   c.offer_challenge(challenges.challenge_1)
+
